3d1ca01c8c4a54ea249e4a76fb5b2b9cca563102
pixelbrackets <mail@pixelbrackets.de>
2014-09-26 18:49:15 +0200

    [FEATURE] Move mail settings into settings file
    Put settings for the notification service (from & to
    email recipients) into a Settings.yaml.

--- Classes/Pixelbrackets/NonWord/Service/Notification.php
+++ Classes/Pixelbrackets/NonWord/Service/Notification.php
@@ -7,6 +7,21 @@ use Pixelbrackets\NonWord\Domain\Model\NonWord;
 class Notification {
 
 	/**
+	* @var array
+	*/
+	protected $settings;
+
+	/**
+	* Inject the settings
+	*
+	* @param array $settings
+	* @return void
+	*/
+	public function injectSettings(array $settings) {
+		$this->settings = $settings;
+	}
+
+	/**
 	 * Send e-mail notification about new non word
 	 *
 	 * @param \Pixelbrackets\NonWord\Domain\Model\NonWord $nonWord
@@ -18,8 +33,8 @@ class Notification {
 			. PHP_EOL . PHP_EOL . $uri;
 
 		$mail = new \TYPO3\SwiftMailer\Message();
-		$mail->setFrom(array('john@example.com ' => 'John Doe'))
-				 ->setTo(array('bill@example.com ' => 'Bill Doe'))
+		$mail->setFrom(array($this->settings['notification']['fromEmail'] => $this->settings['notification']['fromName']))
+				 ->setTo(array($this->settings['notification']['toEmail'] => $this->settings['notification']['toName']))
 				 ->setSubject('New non word')
 				 ->setBody($content)
 				 ->send();
